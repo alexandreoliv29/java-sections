3b1b2cebfdd5c7c934ea7af719a0be76fed3bf6d
alexandreoliv29 <alexandre.foj@gmail.com>
2023-02-15 16:27:50 -0300

    updating README

---
+++ src/MyClass.java
@@ -0,0 +1,24 @@
+public class MyClass {
+    public static void main(String[] args) throws Exception {
+        String nome = "Alexandre";
+        String sobrenome = "Faria de Oliveira Júnior";
+
+        int idade = 21;
+        final String matricula = "190041897";
+
+        System.out.println(nome);
+        System.out.println(sobrenome);
+        System.out.println(idade);
+        System.out.println(matricula);
+
+        boolean autorizacao = true;
+
+        if (autorizacao == true) {
+            System.out.println("tem autorização para sair sozinho");
+        } else {
+
+        }
+
+
+    }
+}
