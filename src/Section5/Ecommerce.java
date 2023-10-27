b7904e458313e81c9bec11fe54cdadb73108ad84
alexandreoliv29 <alexandre.foj@gmail.com>
2023-10-27 14:39:36 -0300

    returning learn java

---
+++ src/Section5/Ecommerce.java
@@ -0,0 +1,14 @@
+package Section5;
+
+import java.util.Scanner;
+
+public class Ecommerce {
+    public void meuMetodo() {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Entre com o nome da pessoa: ");
+        String nome = myScanner.nextLine();
+
+        System.out.println(nome);
+    }
+}
